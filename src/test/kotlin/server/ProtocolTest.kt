1b7b6bb5bcd1257d55baba218cb864244fd9f45c
Claus K <cph-ck83@cphbusiness.dk>
2019-11-09 10:24:44 +0100

    Cleaned up structure so each class has own file.

--- src/test/kotlin/server/ProtocolTest.kt
+++ src/test/kotlin/server/ProtocolTest.kt
@@ -1,8 +1,10 @@
 package server
 
+
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.ByteArrayOutputStream
+import java.lang.StringBuilder
 
 class ProtocolTest
 {
@@ -13,8 +15,8 @@ class ProtocolTest
     """.trimIndent()
     val responseText = """
             HTTP/1.1 200 OK
-            Content-Type: text/html; charset=UTF-8 
-            Content-length: 18
+            Content-Type: text/html; charset=UTF-8
+            Content-length: 18            
             Connection: close
             
             <p>Hello Kurt!</p>
@@ -24,7 +26,6 @@ class ProtocolTest
     fun testRequestResource(){
         val input = requestText.byteInputStream()
         val request = Request(input)
-
         assertEquals("/greeter", request.resource)
     }
 
@@ -42,8 +43,7 @@ class ProtocolTest
         val response = Response(output)
         response.append("<p>Hello Kurt!</p>")
         response.send()
+
         assertEquals(responseText, output.toString())
-
-
     }
 }
