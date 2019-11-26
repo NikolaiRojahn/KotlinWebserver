7c539b7285f1ce65687abf645d1367b90968613b
NikolaiRojahn <cph-nr87@cphbusiness.dk>
2019-11-26 09:38:52 +0100

    Removal of unused commented code

--- src/main/kotlin/server/Reflection.kt
+++ src/main/kotlin/server/Reflection.kt
@@ -3,13 +3,6 @@ package server
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.memberFunctions
 
-/*fun listFunctions(content: Any) {
-    val contentType = content::class
-    println(contentType.simpleName)
-    contentType.memberFunctions.forEach {
-        println(it)
-    }
-}*/
 class Reflection {
     fun callFunction(content: Any, method: Method, resource: String, parameter: Any?) : Any? {
         //println(resource)
@@ -47,8 +40,6 @@ class Reflection {
                     return function.call(content, v1)
                 }
                 MemberDTO::class -> {
-                    //println("HEJ")
-                    //println(parameter)
                     val v1 = parameter
                     return function.call(content, v1)
                 }
@@ -58,30 +49,4 @@ class Reflection {
         else // no parameters required.
             return function.call(content)
     }
-//    fun callFunction(content: Any, method: Method, resource: String) : Any? {
-//        val parts = resource.split("/").filter { !it.isEmpty() }
-//        println(parts)
-//        if (parts.size == 0) return null
-//
-//        val methodName = method.toString().toLowerCase() + (parts[0].capitalize())
-//        println(methodName)
-//        val type = content::class
-//        val function = type.declaredFunctions
-//            .filter { it.name == methodName }
-//            .filter { it.parameters.size == parts.size }
-//            .firstOrNull()
-//        if (function == null) return null
-//        if (function.parameters.size > 1) {
-//            val p = function.parameters[1]
-//            println(p.type.classifier)
-//            when (p.type.classifier) {
-//                Int::class -> {
-//                    val v1 = parts[1].toInt()
-//                    return function.call(content, v1)
-//                }
-//                else -> return null
-//            }
-//        }
-//        else return function.call(content)
-//    }
 }
